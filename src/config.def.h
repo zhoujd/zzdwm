ac4e87a7fcf8bb6cc5805780751fdb353eabcc20
Zachary Zhou <zchrzhou@gmail.com>
2024-10-31 15:08:06 +0800

    dwm: update font/bar/viewontag

--- src/config.def.h
+++ src/config.def.h
@@ -3,11 +3,12 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
+static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int viewontag          = 1;        /* Switch view on tag switch */
 static const int user_bh            = 2;        /* 2 is the default spacing around the bar's font */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "SF Mono:size=10" };
+static const char dmenufont[]       = "SF Mono:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -37,7 +38,6 @@ static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] 
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
-static const Bool viewontag = True;  /* Switch view on tag switch */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
