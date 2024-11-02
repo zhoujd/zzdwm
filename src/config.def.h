e9e689513d8b496558e89d7048fc0c39a4d03767
Zachary Zhou <zchrzhou@gmail.com>
2024-11-02 19:39:47 +0800

    dwm: format comments

--- src/config.def.h
+++ src/config.def.h
@@ -2,12 +2,12 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const int startwithgaps	    = 0;	      /* 1 means gaps are used by default */
+static const int startwithgaps	    = 0;        /* 1 means gaps are used by default */
 static const unsigned int gappx     = 10;       /* default gap between windows in pixels */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int focusonwheel       = 0;
+static const int focusonwheel       = 0;        /* 0 means focus window by mouse click */
 static const int viewontag          = 1;        /* Switch view on tag switch */
 static const int user_bh            = 2;        /* 2 is the default spacing around the bar's font */
 static const int maxmaster          = 4;        /* 4 is the default max number of masters */
