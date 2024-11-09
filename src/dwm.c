a49bd06d69c9eaa8d19ab79f482b44919e7925c3
Zachary Zhou <zchrzhou@gmail.com>
2024-11-09 20:56:58 +0800

    dwm: apply monocle_count patch

--- src/dwm.c
+++ src/dwm.c
@@ -764,6 +764,7 @@ drawbar(Monitor *m)
 	int boxs = drw->fonts->h / 9;
 	int boxw = drw->fonts->h / 6 + 2;
 	unsigned int i, occ = 0, urg = 0;
+	unsigned int a = 0, s = 0;
 	Client *c;
 
 	if (!m->showbar)
@@ -792,6 +793,16 @@ drawbar(Monitor *m)
 				urg & 1 << i);
 		x += w;
 	}
+
+	if (m->lt[m->sellt]->arrange == monocle) {
+		for (c = nexttiled(m->clients), a = 0, s = 0; c; c = nexttiled(c->next), a++)
+			if (c == m->stack)
+				s = a + 1;
+		if (!s && a)
+			s = 1;
+		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d/%d]", s, a);
+	}
+
 	w = TEXTW(m->ltsymbol, 0);
 	drw_setscheme(drw, scheme[SchemeNorm]);
 	x = drw_text(drw, x, 0, w, bh, lrpad / 2, m->ltsymbol, 0, 0);
@@ -1211,8 +1222,6 @@ monocle(Monitor *m)
 	for (c = m->clients; c; c = c->next)
 		if (ISVISIBLE(c))
 			n++;
-	if (n > 0) /* override layout symbol */
-		snprintf(m->ltsymbol, sizeof m->ltsymbol, "[%d]", n);
 	for (c = nexttiled(m->clients); c; c = nexttiled(c->next))
 		if (selmon->drawwithgaps)
 			resize(c, m->wx, m->wy, m->ww - 2 * c->bw, m->wh - 2 * c->bw, 0);
