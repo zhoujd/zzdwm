f565d24cbcdef7a0b350197bd084480ef45c3f4a
Zachary Zhou <zchrzhou@gmail.com>
2024-11-14 17:34:25 +0800

    slock: replace red to black when failed

--- tools/slock/config.def.h
+++ tools/slock/config.def.h
@@ -5,7 +5,7 @@ static const char *group = "nogroup";
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
 	[INPUT] =  "#005577",   /* during input */
-	[FAILED] = "#CC3333",   /* wrong password */
+	[FAILED] = "black",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
