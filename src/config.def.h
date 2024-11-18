f27785f64bae4b896a0be89f28e34964bfe87c9d
Zachary Zhou <zchrzhou@gmail.com>
2024-11-18 22:11:02 +0800

    dwm: update config.def.h

--- src/config.def.h
+++ src/config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const int startwithgaps	    = 0;        /* 1 means gaps are used by default */
+static const int startwithgaps	    = 1;        /* 1 means gaps are used by default */
 static const unsigned int gappx     = 10;       /* default gap between windows in pixels */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int minwsz    = 20;       /* Minimal heigt of a client for smfact */
