e3ae06dc4c264010844be39fbf187445fb400e22
Zachary Zhou <zchrzhou@gmail.com>
2024-11-18 22:22:21 +0800

    dwm: update config.def.h for emacs resize

--- src/config.def.h
+++ src/config.def.h
@@ -59,7 +59,7 @@ static const float smfact    = 0.00; /* factor of tiled clients [0.00..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int nmaxmaster  = 4;    /* max number of clients in master area */
 static const int nminmaster  = 0;    /* min number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
