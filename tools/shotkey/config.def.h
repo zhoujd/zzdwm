1b35125d70115dab0e9f76dfe9381849be06ce87
Zachary Zhou <zchrzhou@gmail.com>
2024-11-19 10:53:02 +0800

    shotkey: update config files

--- tools/shotkey/config.def.h
+++ tools/shotkey/config.def.h
@@ -1,3 +1,7 @@
+/**
+ * config.def.h
+ */
+
 #include <X11/XF86keysym.h>
 
 // Shell
@@ -6,9 +10,8 @@ char shell[] = "/bin/sh";
 #define Super Mod4Mask
 
 enum {
-  Music,
-  Power,
-
+  VolumeControl,
+  Execute,
   // Declare modes above this
   MODE_SIZE,
 };
@@ -16,29 +19,24 @@ enum {
 // Define mode key bindings here
 // NOTE: "10" here is the maximum number of key bindings for each mode
 Key modes[MODE_SIZE][10] = {
-  [Music] = {
-    { 0, XK_h,       cmd("playerctl previous") },
-    { 0, XK_l,       cmd("playerctl next") },
-    { 0, XK_space,   cmd("playerctl play-pause") },
+  [VolumeControl] = {
+    { 0, XK_j, cmd("amixer sset Master '5%-'") },
+    { 0, XK_k, cmd("amixer sset Master '5%+'") },
   },
-  [Power] = {
-    { 0, XK_s,       cmd("notify-send shutting_down") },
-    { 0, XK_r,       cmd("notify-send restarting") },
+  [Execute] = {
+    { 0, XK_c,       cmd("urxvt") },
   },
 };
 
 // Define normal mode key bindings here
 Key keys[] = {
-  { Mod1Mask,  XK_y,             cmd("notify-send hello") },
-  { Mod1Mask,  XK_z,             mode(Music, True) },
-  { Mod1Mask,  XF86XK_PowerOff,  mode(Power, False) },
+  { Super,  XK_y,             mode(VolumeControl, True) },
+  { Super,  XK_a,             mode(Execute, False) },
 };
 
 ModeProperties mode_properties[MODE_SIZE] = {
-  [Music] = { "Music player" },
-  [Power] = { "Power menu" },
+  [Execute] = { "Application Execute" },
 };
 
 // Call this script on mode change
 char* on_mode_change = "echo \"kadj [$SHOTKEY_MODE_ID] $SHOTKEY_MODE_LABEL\"";
-
