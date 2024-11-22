a572efd6130c726aa891c4f60eadd0b48059bb99
Zachary Zhou <zchrzhou@gmail.com>
2024-11-22 13:45:50 +0800

    dmenu: clean comments

--- tools/dmenu/config.def.h
+++ tools/dmenu/config.def.h
@@ -4,8 +4,8 @@
 static int topbar = 1;                         /* -b option; if 0, dmenu appears at bottom     */
 static int centered = 1;                       /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
+static int colorprompt = 1;                    /* if 1, prompt uses SchemeSel, otherwise SchemeNorm */
 static const float menu_height_ratio = 4.0f;   /* This is the ratio used in the original calculation */
-static int colorprompt = 1;                    /* -p  option; if 1, prompt uses SchemeSel, otherwise SchemeNorm */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"SF Mono:size=11",
