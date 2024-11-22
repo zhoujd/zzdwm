2f4e0608c2556fbcbeee243bf1267a013b10ea76
Zachary Zhou <zchrzhou@gmail.com>
2024-11-22 11:21:32 +0800

    dmenu: format config.def.h

--- tools/dmenu/config.def.h
+++ tools/dmenu/config.def.h
@@ -1,16 +1,16 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int topbar = 1;                         /* -b option; if 0, dmenu appears at bottom     */
+static int centered = 1;                       /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
-static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
+static const float menu_height_ratio = 4.0f;   /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"SF Mono:size=11",
 	"SF Mono SC:size=11",
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt      = NULL;         /* -p option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
