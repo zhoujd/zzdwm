20500a56a2ea68625c62da84f0c8ed1675de10c7
Zachary Zhou <zchrzhou@gmail.com>
2024-11-22 13:56:05 +0800

    dmenu: apply bar height patch

--- tools/dmenu/config.def.h
+++ tools/dmenu/config.def.h
@@ -5,6 +5,7 @@ static int topbar = 1;                         /* -b option; if 0, dmenu appears
 static int centered = 1;                       /* -c option; centers dmenu on screen */
 static int min_width = 500;                    /* minimum width when centered */
 static int colorprompt = 1;                    /* if 1, prompt uses SchemeSel, otherwise SchemeNorm */
+static const int user_bh = 0;                  /* add an defined amount of pixels to the bar height */
 static const float menu_height_ratio = 4.0f;   /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
--- tools/dmenu/dmenu.c
+++ tools/dmenu/dmenu.c
@@ -713,7 +713,8 @@ setup(void)
 	utf8 = XInternAtom(dpy, "UTF8_STRING", False);
 
 	/* calculate menu geometry */
-	bh = drw->fonts->h + 2;
+	bh = drw->fonts->h;
+	bh = user_bh ? bh + user_bh : bh + 2;
 	lines = MAX(lines, 0);
 	mh = (lines + 1) * bh;
 	promptw = (prompt && *prompt) ? TEXTW(prompt) - lrpad / 4 : 0;
