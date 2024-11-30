0d6ca9cf5d2d7ddac3c7235a143c0d19e119186a
Zachary Zhou <zchrzhou@gmail.com>
2024-11-30 23:08:45 +0800

    Update on MicroEMACS

---
+++ script/me.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
+CORE_ROOT=$(cd $SCRIPT_ROOT/.. && pwd)
+CORE_TOP=$(cd $CORE_ROOT/.. && pwd)
+
+PROJ_ROOT=$CORE_ROOT/tools/MicroEMACS
+
+dep() {
+    echo "Install dep done"
+}
+
+build() {
+    pushd $PROJ_ROOT
+    make
+    sudo make install
+    popd
+    echo "Build done"
+}
+
+clean() {
+    pushd $PROJ_ROOT
+    git clean -dfx
+    popd
+    echo "Clean done"
+}
+
+usage() {
+    app=$(basename $0)
+    cat <<EOF
+Usage $app {dep|build|-b|clean|-c}
+EOF
+}
+
+case $1 in
+    dep )
+        dep
+        ;;
+    build|-b )
+        build
+        ;;
+    clean|-c )
+        clean
+        ;;
+    * )
+        usage
+        ;;
+esac
