47e3ac855998273548db544d1ef6f73d4e214f98
Zachary Zhou <zchrzhou@gmail.com>
2024-12-02 14:49:41 +0800

    Update docker/run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -4,17 +4,18 @@ SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 BUILD_FILE=$SCRIPT_ROOT/dockerfiles/alpine.dockerfile
 BUILD_ARG=
-IMG=zz/alpine:base
+CNT_IMG=zz/alpine:base
+CNT_NAME=zz-build-1
 
 case $1 in
     build|-b )
-        docker build $BUILD_ARG -t $IMG -f $BUILD_FILE .
+        docker build $BUILD_ARG -t $CNT_IMG -f $BUILD_FILE .
         ;;
     run|-r )
-        docker run -it $IMG
+        docker run -it --name $CNT_NAME $CNT_IMG
         ;;
     status|st )
-        docker ps | grep $IMG
+        docker ps | grep $CNT_IMG
         ;;
     * )
         echo "Usage: $(basename $0) {build|-b|run|-r|status|st}"
