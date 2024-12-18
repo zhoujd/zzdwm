ebcda6308255fb7d0e245bb1ac974ed7cf0b266b
Zachary Zhou <zchrzhou@gmail.com>
2024-12-18 10:51:20 +0800

    dwm: apply gappx to grid layout

--- src/dwm.c
+++ src/dwm.c
@@ -1461,6 +1461,7 @@ monocle(Monitor *m)
 void
 grid(Monitor *m) {
 	unsigned int i, n, cx, cy, cw, ch, aw, ah, cols, rows;
+	int cc, cr, chrest, cwrest;
 	Client *c;
 
 	for(n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next))
@@ -1473,16 +1474,30 @@ grid(Monitor *m) {
 	cols = (rows && (rows - 1) * rows >= n) ? rows - 1 : rows;
 
 	/* window geoms (cell height/width) */
-	ch = m->wh / (rows ? rows : 1);
-	cw = m->ww / (cols ? cols : 1);
-	for(i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next)) {
-		cx = m->wx + (i / rows) * cw;
-		cy = m->wy + (i % rows) * ch;
-		/* adjust height/width of last row/column's windows */
-		ah = ((i + 1) % rows == 0) ? m->wh - ch * rows : 0;
-		aw = (i >= rows * (cols - 1)) ? m->ww - cw * cols : 0;
-		resize(c, cx, cy, cw - 2 * c->bw + aw, ch - 2 * c->bw + ah, False);
-		i++;
+	if (m->drawwithgaps) { /* draw with fullgaps logic */
+		ch = (m->wh - 2*gappx - gappx * (rows - 1)) / (rows ? rows : 1);
+		cw = (m->ww - 2*gappx - gappx * (cols - 1)) / (cols ? cols : 1);
+		chrest = (m->wh - 2*gappx - gappx * (rows - 1)) - ch * rows;
+		cwrest = (m->ww - 2*gappx - gappx * (cols - 1)) - cw * cols;
+		for (i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
+			cc = i / rows;
+			cr = i % rows;
+			cx = m->wx + gappx + cc * (cw + gappx) + MIN(cc, cwrest);
+			cy = m->wy + gappx + cr * (ch + gappx) + MIN(cr, chrest);
+			resize(c, cx, cy, cw + (cc < cwrest ? 1 : 0) - 2*c->bw, ch + (cr < chrest ? 1 : 0) - 2*c->bw, False);
+		}
+	} else {
+		ch = m->wh / (rows ? rows : 1);
+		cw = m->ww / (cols ? cols : 1);
+		for(i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next)) {
+			cx = m->wx + (i / rows) * cw;
+			cy = m->wy + (i % rows) * ch;
+			/* adjust height/width of last row/column's windows */
+			ah = ((i + 1) % rows == 0) ? m->wh - ch * rows : 0;
+			aw = (i >= rows * (cols - 1)) ? m->ww - cw * cols : 0;
+			resize(c, cx, cy, cw - 2 * c->bw + aw, ch - 2 * c->bw + ah, False);
+			i++;
+		}
 	}
 }
 
