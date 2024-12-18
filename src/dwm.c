4bb24232f5af7948e095b0c7161ac6ae167289ea
Zachary Zhou <zchrzhou@gmail.com>
2024-12-18 11:01:47 +0800

    dwm: update grid layout code

--- src/dwm.c
+++ src/dwm.c
@@ -1461,15 +1461,15 @@ monocle(Monitor *m)
 void
 grid(Monitor *m) {
 	unsigned int i, n, cx, cy, cw, ch, aw, ah, cols, rows;
-	int cc, cr, chrest, cwrest;
+	unsigned int cc, cr, chrest, cwrest; //fullgaps logic
 	Client *c;
 
-	for(n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next))
+	for (n = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next))
 		n++;
 
 	/* grid dimensions */
-	for(rows = 0; rows <= n/2; rows++)
-		if(rows*rows >= n)
+	for (rows = 0; rows <= n/2; rows++)
+		if (rows*rows >= n)
 			break;
 	cols = (rows && (rows - 1) * rows >= n) ? rows - 1 : rows;
 
@@ -1489,14 +1489,13 @@ grid(Monitor *m) {
 	} else {
 		ch = m->wh / (rows ? rows : 1);
 		cw = m->ww / (cols ? cols : 1);
-		for(i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next)) {
+		for (i = 0, c = nexttiled(m->clients); c; c = nexttiled(c->next), i++) {
 			cx = m->wx + (i / rows) * cw;
 			cy = m->wy + (i % rows) * ch;
 			/* adjust height/width of last row/column's windows */
 			ah = ((i + 1) % rows == 0) ? m->wh - ch * rows : 0;
 			aw = (i >= rows * (cols - 1)) ? m->ww - cw * cols : 0;
 			resize(c, cx, cy, cw - 2 * c->bw + aw, ch - 2 * c->bw + ah, False);
-			i++;
 		}
 	}
 }
