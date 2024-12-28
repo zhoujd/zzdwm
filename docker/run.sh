46b2eef5401a7d429a2f7ae195467114c802f64a
Zachary Zhou <zchrzhou@gmail.com>
2024-12-28 17:01:26 +0800

    Update docker/run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -4,25 +4,30 @@ SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 
 BUILD_FILE=$SCRIPT_ROOT/dockerfiles/alpine.dockerfile
 BUILD_ARG=
-CNT_IMG=zz/alpine:base
-CNT_NAME=zz-build-1
+IMG=zz/alpine
+TAG=base
+CNT=zz-build-1
 
 case $1 in
     build|-b )
-        docker build $BUILD_ARG -t $CNT_IMG -f $BUILD_FILE .
+        docker build $BUILD_ARG -t $IMG:$TAG -f $BUILD_FILE .
+        docker tag $IMG:$TAG $IMG:latest
         ;;
     run|-r )
-        docker stop $CNT_NAME 2>/dev/null
-        docker rm $CNT_NAME 2>/dev/null
-        docker run -it --name $CNT_NAME $CNT_IMG
+        docker stop $CNT 2>/dev/null
+        docker rm $CNT 2>/dev/null
+        docker run -it --name $CNT $IMG:$TAG
         ;;
     status|-s )
-        docker ps | grep $CNT_IMG
+        docker ps | grep $IMG
         ;;
     clean|-c )
         docker system prune -f
         ;;
+    purge )
+        docker system prune -f -a
+        ;;
     * )
-        echo "Usage: $(basename $0) {build|-b|run|-r|status|-s|clean|-c}"
+        echo "Usage: $(basename $0) {build|-b|run|-r|status|-s|clean|-c|purge}"
         ;;
 esac
