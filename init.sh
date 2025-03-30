3ca0c7469715f9bb928861437a6422db5fea4e8f
Zachary Zhou <zchrzhou@gmail.com>
2025-03-30 14:26:59 +0800

    Update init.sh and bin/ files

--- init.sh
+++ init.sh
@@ -66,9 +66,6 @@ install_bin() {
         $CORE_ROOT/bin/dvtm-status
         $CORE_ROOT/bin/nq
         $CORE_ROOT/bin/nqtail
-        $CORE_ROOT/bin/rsync-avhz
-        $CORE_ROOT/bin/dwmlocal
-        $CORE_ROOT/bin/cwmlocal
     )
     for app in ${apps[@]}; do
         sudo cp -fv $app $target
