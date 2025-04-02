7fb689bff1336f3f26e936cabae54d0ec5e9d470
Zachary Zhou <zchrzhou@gmail.com>
2025-04-02 21:55:28 +0800

    Updte lightdm.sh

--- script/lightdm.sh
+++ script/lightdm.sh
@@ -1,26 +1,57 @@
 #!/bin/bash
 
-echo "Install lighdm"
-sudo apt install -y lightdm
+dep() {
+    echo "Install lightdm"
+    sudo apt install -y lightdm
+    sudo systemctl enable lightdm
 
-echo "Remove default xsession"
-sudo rm -fv /usr/share/xsessions/lightdm-xsession.desktop
+    echo "Remove default xsession"
+    sudo rm -fv /usr/share/xsessions/lightdm-xsession.desktop
+    echo "Install dep done"
+}
 
-echo "Update gtk greeter"
-sudo tee /etc/lightdm/lightdm-gtk-greeter.conf <<EOF
+config() {
+    echo "Update gtk greeter"
+    sudo tee /etc/lightdm/lightdm-gtk-greeter.conf <<EOF
 [greeter]
 theme-name = Adwaita-dark
 icon-theme-name = Adwaita
 clock-format = %a %d %b %I:%M %p
 indicators = ~host;~spacer;~session;~clock;~power
 EOF
+    echo "Enable default users"
+    sudo sed -i 's/#greeter-hide-users=false/greeter-hide-users=false/g' /etc/lightdm/lightdm.conf
+    grep -n greeter-hide-users=false /etc/lightdm/lightdm.conf
+    echo "Config lightdm done"
+}
 
-echo "Enable default users"
-sudo sed -i 's/#greeter-hide-users=false/greeter-hide-users=false/g' /etc/lightdm/lightdm.conf
-grep -n greeter-hide-users=false /etc/lightdm/lightdm.conf
+restart() {
+    sudo systemctl restart lightdm
+    echo "Restart lightdm done"
+}
 
-echo "Restart lightdm"
-sudo systemctl enable lightdm
-sudo systemctl restart lightdm
+usage() {
+    cat <<EOF
+Usage: $(basename $0) {dep|-d|config|-c|restart|-r|all|-a}"
+EOF
+}
 
-echo "Setup lightdm done"
+case $1 in
+    dep|-d )
+        dep
+        ;;
+    config|-c )
+        config
+        ;;
+    restart|-r )
+        restart
+        ;;
+    all|-a )
+        dep
+        config
+        restart
+        ;;
+    * )
+        usage
+        ;;
+esac
