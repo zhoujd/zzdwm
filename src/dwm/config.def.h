ecd095f131f3ab2c87c1bb7fdb2f83c74d2f5779
Zachary Zhou <zchrzhou@gmail.com>
2025-04-17 13:51:59 +0800

    dwm: update config.def.h

--- src/dwm/config.def.h
+++ src/dwm/config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int startwithgaps      = 1;        /* 1 means gaps are used by default */
 static const int gappx              = 5;        /* default gap between windows in pixels */
