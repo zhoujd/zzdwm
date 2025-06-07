7ef7703a764e9c2c54499ffca59c702106468e2b
Zachary Zhou <zchrzhou@gmail.com>
2025-06-07 10:29:29 +0800

    dwm: update config.def.h on nord theme

--- src/dwm/config.def.h
+++ src/dwm/config.def.h
@@ -25,11 +25,11 @@ static const char *fonts[]          = {
  * the index is zero based */
 static const int statusfontindex    = 0;
 /* colors */
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#2e3440";
+static const char col_gray2[]       = "#2e3440";
+static const char col_gray3[]       = "#81a1c1";
+static const char col_gray4[]       = "#e5e9f0";
+static const char col_cyan[]        = "#4c566a";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
