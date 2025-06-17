394654dbb03ea81751e86c4158ac8f5652b27a85
Zachary Zhou <zchrzhou@gmail.com>
2025-06-17 14:34:21 +0800

    dwm: update config.h.def on comments

--- src/dwm/config.def.h
+++ src/dwm/config.def.h
@@ -172,8 +172,8 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_Return,       zoom,             {0} },
 	{ MODKEY,                       XK_Tab,          view,             {0} },
 	{ MODKEY|ShiftMask,             XK_x,            killclient,       {0} },
-	{ MODKEY|ControlMask,           XK_x,            killclient,       {.ui = 1} },
-	{ MODKEY|ControlMask|ShiftMask, XK_x,            killclient,       {.ui = 2} },
+	{ MODKEY|ControlMask,           XK_x,            killclient,       {.ui = 1} }, /* others in tag */
+	{ MODKEY|ControlMask|ShiftMask, XK_x,            killclient,       {.ui = 2} }, /* all in tag */
 	{ MODKEY,                       XK_t,            setlayout,        {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,            setlayout,        {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,            setlayout,        {.v = &layouts[2]} },
