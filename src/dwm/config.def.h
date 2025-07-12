70ba7a4c86f7da9138142592818a6d6e0915fa03
Zachary Zhou <zchrzhou@gmail.com>
2025-07-12 22:02:59 +0800

    dwm: update config.def.h on colors

--- src/dwm/config.def.h
+++ src/dwm/config.def.h
@@ -30,11 +30,11 @@ static const char *fonts[]          = {
  */
 static const int statusfontindex    = 0;
 /* colors */
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#282c34";
+static const char col_gray2[]       = "#282c34";
+static const char col_gray3[]       = "#d7d7d7";
+static const char col_gray4[]       = "#d7d7d7";
+static const char col_cyan[]        = "#924441";
 static const char *colors[][3]      = {
 	/*                fg         bg         border   */
 	[SchemeNorm]  = { col_gray3, col_gray1, col_gray2 },
