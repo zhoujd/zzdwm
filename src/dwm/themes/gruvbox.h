68c9c8f8ad1515906b5cc3b8a7fc8e3dcb4c4d1e
Zachary Zhou <zchrzhou@gmail.com>
2025-07-15 22:59:35 +0800

    dwm: update themes/gruvbox.h

--- src/dwm/themes/gruvbox.h
+++ src/dwm/themes/gruvbox.h
@@ -1,5 +1,5 @@
 /* color */
-static const char col_gray1[]       = "#262626";
+static const char col_gray1[]       = "#282828";
 static const char col_gray2[]       = "#504945";
 static const char col_gray3[]       = "#d5c4a1";
 static const char col_gray4[]       = "#ebdbb2";
