670914ac71e2e462c295de2d2420fdcfe2e6b5b0
Zachary Zhou <zchrzhou@gmail.com>
2025-07-24 17:30:42 +0800

    dmenu: use default color

--- tools/dmenu/themes/gruvbox.h
+++ tools/dmenu/themes/gruvbox.h
@@ -3,7 +3,7 @@
 static const char norm_fg[]      = "#d5c4a1";
 static const char norm_bg[]      = "#282828";
 static const char sel_fg[]       = "#ebdbb2";
-static const char sel_bg[]       = "#076678";
+static const char sel_bg[]       = "#924441";
 static const char out_fg[]       = "#000000";
 static const char out_bg[]       = "#00ffff";
 
