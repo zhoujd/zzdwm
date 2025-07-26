50a84eeb1a5f5d60a7a0b00bb263a4aba4f4fe27
Zachary Zhou <zchrzhou@gmail.com>
2025-07-26 17:32:41 +0800

    dmenu: add nerd colors

---
+++ tools/dmenu/themes/nerd.h
@@ -0,0 +1,13 @@
+/* colors */
+
+static const char norm_fg[]      = "#bbbbbb";
+static const char norm_bg[]      = "#222222";
+static const char sel_fg[]       = "#eeeeee";
+static const char sel_bg[]       = "#4c566a";
+static const char out_fg[]       = "#000000";
+static const char out_bg[]       = "#00ffff";
+
+/* highlight */
+static const char norm_hi_fg[]   = "#ffc978";
+static const char sel_hi_fg[]    = "#ffc978";
+static const char out_hi_fg[]    = "#ffc978";
