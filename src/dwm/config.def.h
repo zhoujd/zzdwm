5ac4fbc59abaeb5f19e7fea6061512aaea096001
Zachary Zhou <zchrzhou@gmail.com>
2025-08-11 14:21:32 +0800

    dwm: update config.def.h on title left align

--- src/dwm/config.def.h
+++ src/dwm/config.def.h
@@ -22,7 +22,7 @@ static const int mincellw           = 200; /* min cell width in grid and resizin
 static const int mincellh           = 120; /* min cell height in grid and resizing in float */
 static const int showstatus         = 2;   /* 0 means no status text, 1 = selected (default), 2 = all monitors */
 static const int floatoffset        = 1;   /* 0 default means no float window offset base on bar height */
-static const int centertitle        = 1;   /* 1 means place title in the middle of barwin, 0 means default */
+static const int centertitle        = 0;   /* 1 means place title in the middle of barwin, 0 means default */
 static const int maxtitle           = 75;  /* max title length in bytes, 0 means no title */
 static const char *fonts[]          = {
 	"SF Mono Nerd:size=11",
