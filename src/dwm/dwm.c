fb45d6f51b9987a5ba8c6426cc438bf234481fc1
Zachary Zhou <zchrzhou@gmail.com>
2025-08-19 23:03:42 +0800

    dwm: apply swaptags patch

--- src/dwm/dwm.c
+++ src/dwm/dwm.c
@@ -252,6 +252,7 @@ static void shiftviewclients(const Arg *arg);
 static void showhide(Client *c);
 static void spawn(const Arg *arg);
 static void swapfocus();
+static void swaptags(const Arg *arg);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
 static void tagswapmon(const Arg *arg);
@@ -2760,6 +2761,28 @@ swapfocus()
 		focus(prevclient);
 		restack(prevclient->mon);
 	}
+}
+
+void
+swaptags(const Arg *arg)
+{
+	unsigned int newtag = arg->ui & TAGMASK;
+	unsigned int curtag = selmon->tagset[selmon->seltags];
+
+	if (newtag == curtag || !curtag || (curtag & (curtag-1)))
+		return;
+
+	for (Client *c = selmon->clients; c != NULL; c = c->next) {
+		if((c->tags & newtag) || (c->tags & curtag))
+			c->tags ^= curtag ^ newtag;
+
+		if(!c->tags) c->tags = newtag;
+	}
+
+	selmon->tagset[selmon->seltags] = newtag;
+
+	focus(NULL);
+	arrange(selmon);
 }
 
 void
