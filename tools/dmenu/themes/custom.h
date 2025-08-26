419cd95b71b10485300a26bf80d2820735bc3dcd
Zachary Zhou <zchrzhou@gmail.com>
2025-08-26 15:20:03 +0800

    dmenu: update themes/custom.h on Nord colors

--- tools/dmenu/themes/custom.h
+++ tools/dmenu/themes/custom.h
@@ -1,7 +1,7 @@
 /* colors */
-static const char norm_fg[]      = "#bbbbbb";
-static const char norm_bg[]      = "#222222";
-static const char sel_fg[]       = "#eeeeee";
+static const char norm_fg[]      = "#d8dee9";
+static const char norm_bg[]      = "#242933";
+static const char sel_fg[]       = "#e5e9f0";
 static const char sel_bg[]       = "#4c566a";
 static const char out_fg[]       = "#000000";
 static const char out_bg[]       = "#00ffff";
