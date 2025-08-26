763d63478f809a4792d4efd9f754c58381c05f8a
Zachary Zhou <zchrzhou@gmail.com>
2025-08-26 10:08:19 +0800

    dwm: update config.def.h on font

--- src/dwm/config.def.h
+++ src/dwm/config.def.h
@@ -25,7 +25,7 @@ static const int floatoffset        = 1;   /* 0 default means no float window of
 static const int centertitle        = 1;   /* 1 means place title in the middle of barwin, 0 means default */
 static const int maxtitle           = 75;  /* max title length in bytes, 0 means no title */
 static const char *fonts[]          = {
-	"Spleen:size=11",
+	"SF Mono Nerd:size=11",
 	"Droid Sans Fallback:size=11",
 };
 /* select the font index for statusbar
