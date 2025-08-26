cc7cf99d7ec9a108d45c2863c9ba863fbbfef8e6
Zachary Zhou <zchrzhou@gmail.com>
2025-08-26 14:56:12 +0800

    dwm: update themes/custom.h

--- src/dwm/themes/custom.h
+++ src/dwm/themes/custom.h
@@ -1,5 +1,5 @@
 /* color */
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#242933";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
