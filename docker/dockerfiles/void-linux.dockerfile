2d853fee9dd73818021533eac9bd810b7ec645fc
Zachary Zhou <zchrzhou@gmail.com>
2025-09-12 10:42:45 +0800

    Update docker files

--- docker/dockerfiles/void-linux.dockerfile
+++ docker/dockerfiles/void-linux.dockerfile
@@ -1,6 +1,6 @@
 FROM ghcr.io/void-linux/void-musl
 RUN xbps-install -Sy make gcc libtool autoconf automake pkg-config \
-        wget ncurses ncurses-devel file upx tar gzip bzip2 openssh \
+        wget ncurses ncurses-devel file upx tar gzip bzip2 openssh git \
         && xbps-remove -Oo
 USER root
 CMD ["/bin/sh"]
--- docker/run.sh
+++ docker/run.sh
@@ -1,34 +1,67 @@
 #!/bin/bash
 
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
-
-IMG=zhoujd/alpine
-#IMG=zhoujd/void-linux
-TAG=base
 CNT=zz-build-1
+
+run() {
+    local kind=$1
+    local img=zhoujd/alpine
+    local tag=base
+    case $kind in
+        alpine|-a )
+            img=zhoujd/alpine
+            ;;
+        void|-v )
+            img=zhoujd/void-linux
+            ;;
+    esac
+    docker stop $CNT >/dev/null 2>&1
+    docker rm $CNT >/dev/null 2>&1
+    docker run -it --name $CNT $img:$tag
+    echo "Run Done"
+}
+
+status() {
+    echo "IMG:"
+    docker images | grep zhoujd/alpine
+    docker images | grep zhoujd/void-linux
+    echo "PS:"
+    docker ps -a | grep $CNT
+    echo "Status Done"
+}
+
+clean() {
+    docker stop $CNT >/dev/null 2>&1
+    docker rm $CNT >/dev/null 2>&1
+    echo "Clean Done"
+}
+
+usage() {
+    local app=$(basename $0)
+    cat <<EOF
+Usage: $app {build|-b|run|-r|status|-s|clean|-c|purge}
+run|-r   --   {alpine|-a|void|-v}
+EOF
+}
 
 case $1 in
     build|-b )
         make -C dockerfiles
         ;;
     run|-r )
-        docker stop $CNT >/dev/null 2>&1
-        docker rm $CNT >/dev/null 2>&1
-        docker run -it --name $CNT $IMG:$TAG
+        shift
+        run "$@"
         ;;
     status|-s )
-        echo "IMG:"
-        docker images | grep $IMG
-        echo "PS:"
-        docker ps -a | grep $IMG
+        status
         ;;
     clean|-c )
-        docker system prune -f
+        clean
         ;;
     purge )
         docker system prune -f -a
         ;;
     * )
-        echo "Usage: $(basename $0) {build|-b|run|-r|status|-s|clean|-c|purge}"
+        usage
         ;;
 esac
