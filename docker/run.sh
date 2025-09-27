0c382492be0b515cb482a566b3a1131c41b548cd
Zachary Zhou <zchrzhou@gmail.com>
2025-09-27 22:29:14 +0800

    Update docker/run.sh

--- docker/run.sh
+++ docker/run.sh
@@ -3,17 +3,14 @@
 SCRIPT_ROOT=$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)
 TOP=$(cd $SCRIPT_ROOT/.. && pwd)
 CTN=zz-build-1
+MNT=/root
 IMGS=(
-    zhoujd/alpine
-    zhoujd/void-linux
+    zhoujd/alpine:base
+    zhoujd/void-linux:base
 )
 
 run() {
     local kind=$1
-    local idx=0
-    local tag=base
-    local mnt=/root
-    local img=${IMGS[0]}
     case $kind in
         alpine|-a )
             img=${IMGS[0]}
@@ -21,13 +18,17 @@ run() {
         void|-v )
             img=${IMGS[1]}
             ;;
+        * )
+            img=${IMGS[0]}
+            ;;
     esac
     docker stop $CTN >/dev/null 2>&1
     docker rm $CTN >/dev/null 2>&1
     docker run \
-           -it --name $CTN \
-           -v $TOP:$mnt/$(basename $TOP) \
-           $img:$tag
+           -it \
+           --name $CTN \
+           -v $TOP:$MNT/$(basename $TOP) \
+           $img
     echo "Run Done"
 }
 
