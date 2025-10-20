228ca04d091a46070703f14c4a9763c4a5b77c6c
Zachary Zhou <zchrzhou@gmail.com>
2025-10-20 11:07:02 +0800

    Update on bin/ff

--- init.sh
+++ init.sh
@@ -44,7 +44,6 @@ install_bin() {
         $CORE_ROOT/bin/me
         $CORE_ROOT/bin/mg
         $CORE_ROOT/bin/vi
-        $CORE_ROOT/bin/ff
         $CORE_ROOT/bin/movewin
         $CORE_ROOT/bin/deckwin
         $CORE_ROOT/bin/focuswin
