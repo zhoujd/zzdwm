12e251beea84cda1ff1bd2d9f6581ba13b106374
Zachary Zhou <zchrzhou@gmail.com>
2025-10-25 23:24:02 +0800

    ME: update .dir-locals.el

--- tools/MicroEMACS/.dir-locals.el
+++ tools/MicroEMACS/.dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((c-mode
-  (indent-tabs-mode . nil)))
+((c-mode . ((indent-tabs-mode . nil)
+            (c-file-style . "gnu"))))
