2a528262f90b45d3a1b4b879e583b784a5a279d7
Zachary Zhou <zchrzhou@gmail.com>
2025-10-26 23:16:52 +0800

    ME: update bufmenu.c on format

--- tools/MicroEMACS/bufmenu.c
+++ tools/MicroEMACS/bufmenu.c
@@ -50,192 +50,192 @@ int count_buffers(void);
 int
 buffermenu(int f, int n)
 {
-	BUFFER *bp;
-	BUFFER *org_bp = curbp;
-	int c,k;
-	int bufptr;
-	int bufcount = 0;
+  BUFFER *bp;
+  BUFFER *org_bp = curbp;
+  int c,k;
+  int bufptr;
+  int bufcount = 0;
 
-	bufptr = 1;
+  bufptr = 1;
 
-start:
-	listbuffers(f, n, KRANDOM);
-	swbuffer(blistp);
-	onlywind(0, 0, KRANDOM);
-	bufcount = count_buffers();
+ start:
+  listbuffers(f, n, KRANDOM);
+  swbuffer(blistp);
+  onlywind(0, 0, KRANDOM);
+  bufcount = count_buffers();
 
-	if (bufptr > bufcount)
-		bufptr = bufcount;
+  if (bufptr > bufcount)
+    bufptr = bufcount;
 
-	if (bufcount > 0)
-		forwline(0, bufptr + 1, KRANDOM);
-	else
-		forwline(0, 2, KRANDOM);
+  if (bufcount > 0)
+    forwline(0, bufptr + 1, KRANDOM);
+  else
+    forwline(0, 2, KRANDOM);
 
-	for (;;)
-	{
-		eprintf("Buffer Menu: 1,2,s,v,k,q ");
-		update();
-		c = ttgetc();
+  for (;;)
+    {
+      eprintf("Buffer Menu: 1,2,s,v,k,q ");
+      update();
+      c = ttgetc();
 
-		/* if no buffers, only allow exit */
-		if (bufcount == 0)
-		{
-			switch (c)
-			{
-			case 'q': case 'Q': case 'x': case 'X':
-				break;
-			default:
-				ttbeep();
-				continue;
-			}
-		}
+      /* if no buffers, only allow exit */
+      if (bufcount == 0)
+        {
+          switch (c)
+            {
+            case 'q': case 'Q': case 'x': case 'X':
+              break;
+            default:
+              ttbeep();
+              continue;
+            }
+        }
 
-		/*
-		 * pre process escape sequence to get up/down arrows
-		 * convert to CTRL+N, CTRL+P
-		 */
-		if (c == ESC)
-		{
-			k = getctl();
-			if (k == '[')
-			{
-				k = getctl();
-				switch(k)
-				{
-				case 'A': c = CTRL_P; break;
-				case 'B': c = CTRL_N; break;
-				default:
-					ttbeep();
-					continue;
-				}
-			}
-			else
-			{
-				k = getctl();
-				ttbeep();
-				continue;
-			}
-		}  /* if ESC */
+      /*
+       * pre process escape sequence to get up/down arrows
+       * convert to CTRL+N, CTRL+P
+       */
+      if (c == ESC)
+        {
+          k = getctl();
+          if (k == '[')
+            {
+              k = getctl();
+              switch(k)
+                {
+                case 'A': c = CTRL_P; break;
+                case 'B': c = CTRL_N; break;
+                default:
+                  ttbeep();
+                  continue;
+                }
+            }
+          else
+            {
+              k = getctl();
+              ttbeep();
+              continue;
+            }
+        }  /* if ESC */
 
-		switch (c)
-		{
-		case 'n':
-		case 'N':
-		case ' ':
-		case CTRL_N:
-			if (bufcount == bufptr)
-			{
-				ttbeep();
-				break;
-			}
-			forwline(0, 1, KRANDOM);
-			bufptr++;
-			break;
+      switch (c)
+        {
+        case 'n':
+        case 'N':
+        case ' ':
+        case CTRL_N:
+          if (bufcount == bufptr)
+            {
+              ttbeep();
+              break;
+            }
+          forwline(0, 1, KRANDOM);
+          bufptr++;
+          break;
 
-		case 'p':
-		case 'P':
-		case CTRL_P:
-		case BACKSPACE:
-			if (bufptr == 1)
-			{
-				ttbeep();
-				break;
-			}
-			backline(0, 1, KRANDOM);
-			bufptr--;
-			break;
+        case 'p':
+        case 'P':
+        case CTRL_P:
+        case BACKSPACE:
+          if (bufptr == 1)
+            {
+              ttbeep();
+              break;
+            }
+          backline(0, 1, KRANDOM);
+          bufptr--;
+          break;
 
-		case '1':
-		case CTRL_M:
-			bp = get_buffer(bufptr);
-			swbuffer(bp);
-			onlywind(0, 0, KRANDOM);
-			eerase();
-			return TRUE;
+        case '1':
+        case CTRL_M:
+          bp = get_buffer(bufptr);
+          swbuffer(bp);
+          onlywind(0, 0, KRANDOM);
+          eerase();
+          return TRUE;
 
-		case '2':
-			bp = get_buffer(bufptr);
-			swbuffer(bp);
-			onlywind(0, 0, KRANDOM);
-			/* need to check or is still valid */
-			if (valid_buf(org_bp) == TRUE && bufcount > 1)
-			{
-				splitwind(0, 0, KRANDOM);
-				swbuffer(org_bp);
-				nextwind(0, 0, KRANDOM);
-			}
-			eerase();
-			return TRUE;
+        case '2':
+          bp = get_buffer(bufptr);
+          swbuffer(bp);
+          onlywind(0, 0, KRANDOM);
+          /* need to check or is still valid */
+          if (valid_buf(org_bp) == TRUE && bufcount > 1)
+            {
+              splitwind(0, 0, KRANDOM);
+              swbuffer(org_bp);
+              nextwind(0, 0, KRANDOM);
+            }
+          eerase();
+          return TRUE;
 
-			/* save file */
-		case 's':
-		case 'S':
-			bp = get_buffer(bufptr);
-			if (bp != NULL)
-			{
-				curbp = bp;
-				(void)filesave(0, 0, KRANDOM);
-				curbp = blistp;
-				goto start;
-			}
-			break;
+          /* save file */
+        case 's':
+        case 'S':
+          bp = get_buffer(bufptr);
+          if (bp != NULL)
+            {
+              curbp = bp;
+              (void)filesave(0, 0, KRANDOM);
+              curbp = blistp;
+              goto start;
+            }
+          break;
 
-			/* toggle read only */
-		case 'v':
-		case 'V':
-		case '%':
-			bp = get_buffer(bufptr);
-			if (bp != NULL)  /* be defensive */
-				bp->b_flag ^= BFRO;
-			goto start;
-			break;
+          /* toggle read only */
+        case 'v':
+        case 'V':
+        case '%':
+          bp = get_buffer(bufptr);
+          if (bp != NULL)  /* be defensive */
+            bp->b_flag ^= BFRO;
+          goto start;
+          break;
 
-			/* kill buffer */
-		case 'd':
-		case 'D':
-		case 'k':
-		case 'K':
-			bp = get_buffer(bufptr);
-			if (bp != NULL)
-				zotbuf(bp);
-			goto start;
-			break;
+          /* kill buffer */
+        case 'd':
+        case 'D':
+        case 'k':
+        case 'K':
+          bp = get_buffer(bufptr);
+          if (bp != NULL)
+            zotbuf(bp);
+          goto start;
+          break;
 
-			/* exit buffer menu */
-		case 'q':
-		case 'Q':
-		case 'x':
-		case 'X':
-			if (bufcount == 0)
-			{
-				bp = get_scratch();
-				swbuffer(bp);
-				onlywind(0, 0, KRANDOM);
-				eerase();
-				return TRUE;
-			}
+          /* exit buffer menu */
+        case 'q':
+        case 'Q':
+        case 'x':
+        case 'X':
+          if (bufcount == 0)
+            {
+              bp = get_scratch();
+              swbuffer(bp);
+              onlywind(0, 0, KRANDOM);
+              eerase();
+              return TRUE;
+            }
 
-			if (valid_buf(org_bp) == TRUE)
-				swbuffer(org_bp);
-			else
-			{
-				bp = get_scratch();
-				swbuffer(bp);
-			}
-			onlywind(0, 0, KRANDOM);
-			eerase();
-			return TRUE;
+          if (valid_buf(org_bp) == TRUE)
+            swbuffer(org_bp);
+          else
+            {
+              bp = get_scratch();
+              swbuffer(bp);
+            }
+          onlywind(0, 0, KRANDOM);
+          eerase();
+          return TRUE;
 
-			/* any other key */
-		default:
-			ttbeep();
-			break;
-		}
-	}
+          /* any other key */
+        default:
+          ttbeep();
+          break;
+        }
+    }
 
-	eerase();
-	return TRUE;
+  eerase();
+  return TRUE;
 }
 
 /*
@@ -247,28 +247,28 @@ start:
 BUFFER*
 get_buffer(int n)
 {
-	BUFFER *bp;
-	int i = 0;
+  BUFFER *bp;
+  int i = 0;
 
-	bp = bheadp;
-	while (bp != NULL)
-	{
-		if ((bp->b_flag & BFTEMP) != 0)
-		{
-			bp = bp->b_bufp;
-			continue;
-		}
+  bp = bheadp;
+  while (bp != NULL)
+    {
+      if ((bp->b_flag & BFTEMP) != 0)
+        {
+          bp = bp->b_bufp;
+          continue;
+        }
 
-		if (++i == n)
-			return bp;
+      if (++i == n)
+        return bp;
 
-		bp = bp->b_bufp;
-	}
+      bp = bp->b_bufp;
+    }
 
-	/* we should never get here */
-	eprintf("[Fatal: could not find buffer]");
-	exit(1);
-	return NULL;
+  /* we should never get here */
+  eprintf("[Fatal: could not find buffer]");
+  exit(1);
+  return NULL;
 }
 
 
@@ -280,41 +280,41 @@ get_buffer(int n)
 int
 count_buffers(void)
 {
-	BUFFER *bp;
-	int count = 0;
+  BUFFER *bp;
+  int count = 0;
 
-	bp = bheadp;
-	while (bp != NULL)
-	{
-		if ((bp->b_flag & BFTEMP) != 0)
-		{
-			bp = bp->b_bufp;
-			continue;
-		}
-		count++;
-		bp = bp->b_bufp;
-	}
+  bp = bheadp;
+  while (bp != NULL)
+    {
+      if ((bp->b_flag & BFTEMP) != 0)
+        {
+          bp = bp->b_bufp;
+          continue;
+        }
+      count++;
+      bp = bp->b_bufp;
+    }
 
-	return count;
+  return count;
 }
 
 /*
- * check that the buffer is still in use and in the 
+ * check that the buffer is still in use and in the
  * list of known buffers
  */
 int
 valid_buf(BUFFER* bp_try)
 {
-	BUFFER *bp;
+  BUFFER *bp;
 
-	bp = bheadp;
-	while (bp != NULL)
-	{
-		if (bp == bp_try)
-			return TRUE;  /* we found it */
-		bp = bp->b_bufp;
-	}
+  bp = bheadp;
+  while (bp != NULL)
+    {
+      if (bp == bp_try)
+        return TRUE;  /* we found it */
+      bp = bp->b_bufp;
+    }
 
-	/* if we get here the buffer must have been killed */
-	return (FALSE);
+  /* if we get here the buffer must have been killed */
+  return (FALSE);
 }
