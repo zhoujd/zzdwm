409c9047d730e0929ccb1753a8d1bd82c7e46ab2
Zachary Zhou <zchrzhou@gmail.com>
2025-11-28 14:06:24 +0800

    dwm: update custom.h

--- src/dwm/themes/custom.h
+++ src/dwm/themes/custom.h
@@ -1,6 +1,1 @@
-/* color */
-static const char col_gray1[]       = "#242933";
-static const char col_gray2[]       = "#3b4252";
-static const char col_gray3[]       = "#d8dee9";
-static const char col_gray4[]       = "#e5e9f0";
-static const char col_cyan[]        = "#4c566a";
+nord.h
