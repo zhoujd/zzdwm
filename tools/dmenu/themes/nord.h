461a74c3994e90c6d9e3f4cdd9e6a8a93eaf1dcc
Zachary Zhou <zchrzhou@gmail.com>
2025-11-28 14:08:29 +0800

    dmenu: update custom.h

---
+++ tools/dmenu/themes/nord.h
@@ -0,0 +1,12 @@
+/* colors */
+static const char norm_fg[]      = "#d8dee9";
+static const char norm_bg[]      = "#242933";
+static const char sel_fg[]       = "#e5e9f0";
+static const char sel_bg[]       = "#4c566a";
+static const char out_fg[]       = "#000000";
+static const char out_bg[]       = "#00ffff";
+
+/* highlight */
+static const char norm_hi_fg[]   = "#ffc978";
+static const char sel_hi_fg[]    = "#ffc978";
+static const char out_hi_fg[]    = "#ffc978";
