08d01285720a4752a78709cbf6f26d3387f9d599
Zachary Zhou <zchrzhou@gmail.com>
2026-01-07 22:18:34 +0800

    mg: add build.sh

---
+++ tools/mg/build.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+MNT_DIR=$(cd $SCRIPT_DIR/../.. && pwd)
+
+build() {
+    make
+    echo "Build done"
+}
+
+release() {
+    make clean
+    make LDFLAGS=-static
+    strip -v mg
+    echo "Release done"
+}
+
+publish() {
+    if [ -n "$INSIDE_DOCKER" ]; then
+        echo "Publish in docker run release"
+        release
+        exit
+    fi
+    img=zhoujd/alpine
+    opt="
+        -i \
+        -u $(id -u):$(id -g) \
+        -v $MNT_DIR:$MNT_DIR \
+        -w $MNT_DIR/tools/mg \
+    "
+    docker run $opt $img sh <<'EOF'
+make clean
+make LDFLAGS=-static
+strip -v mg
+EOF
+    echo "Publish done"
+}
+
+clean() {
+    make clean
+    echo "Clean done"
+}
+
+install() {
+    if [ "$(whoami)" == "root" ]; then
+        make install
+    else
+        sudo make install
+    fi
+    echo "Install done"
+}
+
+uninstall() {
+    if [ "$(whoami)" == "root" ]; then
+        make uninstall
+    else
+        sudo make uninstall
+    fi
+    echo "Uninstall done"
+}
+
+usage() {
+    app=$(basename $0)
+    cat <<EOF
+usage: $app {build|-b|release|-r|publish|-p|clean|-c|install|-i|uninstall|-u}
+EOF
+}
+
+case $1 in
+    build|-b )
+        build
+        ;;
+    release|-r )
+        release
+        ;;
+    publish|-p )
+        publish
+        ;;
+    clean|-c )
+        clean
+        ;;
+    install|-i )
+        install
+        ;;
+    uninstall|-u )
+        uninstall
+        ;;
+    * )
+        usage
+        ;;
+esac
