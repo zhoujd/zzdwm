d983f7b86db25a063cad22e3105eecfbbc548545
Zachary Zhou <zchrzhou@gmail.com>
2026-02-23 12:52:34 +0800

    Add bin/pick

--- init.sh
+++ init.sh
@@ -90,8 +90,8 @@ install_bin() {
         $CORE_ROOT/bin/sc
         $CORE_ROOT/bin/ag
         $CORE_ROOT/bin/vi
-        $CORE_ROOT/bin/fzy
         $CORE_ROOT/bin/less
+        $CORE_ROOT/bin/pick
         $CORE_ROOT/bin/etags
         $CORE_ROOT/bin/screen
         $CORE_ROOT/bin/busybox
