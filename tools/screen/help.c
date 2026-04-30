b7b16e525c1a2887fbc5d01c31038e0c1ac501b2
Zachary Zhou <zchrzhou@gmail.com>
2026-04-30 16:16:40 +0800

    screen: update help.c

--- tools/screen/help.c
+++ tools/screen/help.c
@@ -466,32 +466,7 @@ static struct LayFuncs CopyrightLf = {
 
 static const char cpmsg[] = "\
 \n\
-GNU Screen version %v\n\
-\n\
-Copyright (c) 2018-2020 Alexander Naumov, Amadeusz Slawinski\n\
-Copyright (c) 2015-2017 Juergen Weigert, Alexander Naumov, Amadeusz Slawinski\n\
-Copyright (c) 2010-2014 Juergen Weigert, Sadrul Habib Chowdhury\n\
-Copyright (c) 2008-2009 Juergen Weigert, Michael Schroeder, Micah Cowan, Sadrul Habib Chowdhury\n\
-Copyright (c) 1993-2007 Juergen Weigert, Michael Schroeder\n\
-Copyright (c) 1987 Oliver Laumann\n\
-\n\
-This program is free software; you can redistribute it and/or \
-modify it under the terms of the GNU General Public License as published \
-by the Free Software Foundation; either version 3, or (at your option) \
-any later version.\n\
-\n\
-This program is distributed in the hope that it will be useful, \
-but WITHOUT ANY WARRANTY; without even the implied warranty of \
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the \
-GNU General Public License for more details.\n\
-\n\
-You should have received a copy of the GNU General Public License \
-along with this program (see the file COPYING); if not, see \
-https://www.gnu.org/licenses/, or contact Free Software Foundation, Inc., \
-51 Franklin Street, Fifth Floor, Boston, MA  02111-1301  USA.\n\
-\n\
-Send bugreports, fixes, enhancements, t-shirts, money, beer & pizza to \
-screen-devel@gnu.org\n\n\n"
+GNU Screen version %v\n\n\n"
 
 "Capabilities:\n"
 
