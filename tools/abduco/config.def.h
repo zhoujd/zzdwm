8b6f46e35f544c11698c17b41af0e5acbc4dd8ca
Zachary Zhou <zchrzhou@gmail.com>
2026-05-21 12:55:50 +0800

    abduco: update config.def.h on default prefix

--- tools/abduco/config.def.h
+++ tools/abduco/config.def.h
@@ -1,7 +1,7 @@
 /* default command to execute if non is given and $ABDUCO_CMD is unset */
 #define ABDUCO_CMD "bash"
 /* default detach key, can be overriden at run time using -e option */
-static char KEY_DETACH = CTRL('\\');
+static char KEY_DETACH = CTRL('^');
 /* redraw key to send a SIGWINCH signal to underlying process
  * (set to 0 to disable the redraw key) */
 static char KEY_REDRAW = 0;
