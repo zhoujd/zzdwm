2604894951783157c0e5377d12fbf81769ddaa18
Zachary Zhou <zchrzhou@gmail.com>
2026-05-23 16:52:00 +0800

    dvtm: update config.mk

--- tools/dvtm/config.mk
+++ tools/dvtm/config.mk
@@ -12,6 +12,7 @@ TERMINFO := ${DESTDIR}${PREFIX}/share/terminfo
 INCS = -I.
 LIBS = -lc -lutil -lncursesw
 CPPFLAGS = -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700 -D_XOPEN_SOURCE_EXTENDED
-CFLAGS += -std=c99 ${INCS} -DVERSION=\"${VERSION}\" -DNDEBUG ${CPPFLAGS} -Wno-stringop-overread
+CFLAGS += -std=c99 ${INCS} -DVERSION=\"${VERSION}\" -DNDEBUG ${CPPFLAGS} \
+          -Wno-stringop-overread
 
 CC ?= cc
