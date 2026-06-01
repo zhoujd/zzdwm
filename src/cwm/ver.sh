d67ef55d5c764c5321dde6dd0d71ec03433cb357
Zachary Zhou <zchrzhou@gmail.com>
2026-06-01 23:51:16 +0800

    cwm: update ver.sh

--- src/cwm/ver.sh
+++ src/cwm/ver.sh
@@ -2,11 +2,8 @@
 
 h=${1:-"ver.h"}
 
-# Helper function
-available() { command -v "${1:?}" >/dev/null; }
-
 echo "#define DATE \"`date +%Y-%m-%d`\"" >$h
-if available git ; then
+if command -v git >/dev/null; then
   git rev-parse HEAD | sed -n 's/^\(.......\).*/#define REV "git-\1"/p' >>$h
 else
   echo "#define REV \"unknown\"" >>$h
