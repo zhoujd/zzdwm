b76b6301d513f1a884de39d9fd0886a8cb2f209d
Zachary Zhou <zchrzhou@gmail.com>
2026-06-02 12:53:24 +0800

    dwm: update gruvbox.h

--- src/dwm/themes/gruvbox.h
+++ src/dwm/themes/gruvbox.h
@@ -3,4 +3,4 @@ static const char col_gray1[]       = "#282828"; /* norm/status bg      */
 static const char col_gray2[]       = "#3c3836"; /* norm/status border  */
 static const char col_gray3[]       = "#d5c4a1"; /* norm/status fg      */
 static const char col_gray4[]       = "#ebdbb2"; /* sel/title fg        */
-static const char col_cyan[]        = "#924441"; /* sel/title bg/border */
+static const char col_cyan[]        = "#665c54"; /* sel/title bg/border */
