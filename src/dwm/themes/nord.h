84298e84bf690e3e9554b3e377ecc2494ad086ff
Zachary Zhou <zchrzhou@gmail.com>
2026-06-02 12:25:59 +0800

    dwm: update nord theme

--- src/dwm/themes/nord.h
+++ src/dwm/themes/nord.h
@@ -1,6 +1,6 @@
 /* color */
-static const char col_gray1[]       = "#242933";
-static const char col_gray2[]       = "#3b4252";
-static const char col_gray3[]       = "#d8dee9";
-static const char col_gray4[]       = "#e5e9f0";
-static const char col_cyan[]        = "#4c566a";
+static const char col_gray1[]       = "#242933"; /* Bar background */
+static const char col_gray2[]       = "#3b4252"; /* Inactive border */
+static const char col_gray3[]       = "#d8dee9"; /* Inactive text */
+static const char col_gray4[]       = "#e5e9f0"; /* Active text */
+static const char col_cyan[]        = "#4c566a"; /* Active border / Bar accent */
