120e4796fbc73a2f6efe72eee8b59d49719a21fa
Zachary Zhou <zchrzhou@gmail.com>
2026-06-18 14:58:15 +0800

    mg: update build.sh

--- tools/editor/mg/build.sh
+++ tools/editor/mg/build.sh
@@ -32,6 +32,8 @@ publish() {
     else
         img=zhoujd/alpine
         opt="
+            --name=build-mg-1
+            --rm
             -i
             -u $(id -u):$(id -g)
             -v $MNT_DIR:$MNT_DIR
