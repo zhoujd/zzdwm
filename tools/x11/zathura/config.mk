5de76b1f170df6f1b95b4fc8bb336fd3ec736d23
Zachary Zhou <zchrzhou@gmail.com>
2026-06-26 23:33:28 +0800

    zathura: init create

---
+++ tools/x11/zathura/config.mk
@@ -0,0 +1,31 @@
+# See LICENSE file for license and copyright information
+# zathura make config
+
+VERSION = 0.0.8.4
+
+# paths
+PREFIX ?= /usr
+MANPREFIX ?= ${PREFIX}/share/man
+DESKTOPPREFIX ?= ${PREFIX}/share/applications
+
+# libs
+GTK_INC = $(shell pkg-config --cflags gtk+-2.0 poppler-glib)
+GTK_LIB = $(shell pkg-config --libs gtk+-2.0 gthread-2.0 poppler-glib)
+
+INCS = -I. -I/usr/include ${GTK_INC}
+LIBS = -lc ${GTK_LIB} -lpthread -lm
+
+# compiler flags
+CFLAGS += -std=c99 -pedantic -Wall $(INCS)
+
+# debug flags
+DFLAGS = -g
+
+# linker flags
+LDFLAGS ?=
+
+# compiler
+CC ?= gcc
+
+# strip
+SFLAGS ?= -s
