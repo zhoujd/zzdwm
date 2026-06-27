3a73158ad81aaabd0fb7f1f33db2e85d28a661f8
Zachary Zhou <zchrzhou@gmail.com>
2026-06-27 09:46:10 +0800

    zathura: update build.sh

--- tools/x11/zathura/build.sh
+++ tools/x11/zathura/build.sh
@@ -2,11 +2,18 @@
 
 depend() {
   sudo apt install -y libpoppler-glib-dev
+  echo "Install depend done"
 }
 
 build() {
   make
   sudo make install
+  echo "Build done"
+}
+
+clean() {
+  make clean
+  echo "Clean done"
 }
 
 case $1 in
@@ -16,7 +23,10 @@ case $1 in
   build|-b )
     build
     ;;
+  clean|-c )
+    clean
+    ;;
   * )
-    echo $(basename $0) {dep}
+    echo "$(basename $0) {dep|build|-b|clean|-c}"
     ;;
 esac
