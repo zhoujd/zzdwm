6a5b4b5a755dc81b1753486f1474c87b0747f04f
Zachary Zhou <zchrzhou@gmail.com>
2026-08-06 17:19:28 +0800

    Rename libexec/dvtm/dvtm-opener -> libexec/dvtm/dvtm-open

--- init.sh
+++ init.sh
@@ -115,7 +115,7 @@ install_bin() {
         $CORE_ROOT/libexec/dvtm/dvtm-pager
         $CORE_ROOT/libexec/dvtm/dvtm-status
         $CORE_ROOT/libexec/dvtm/dvtm-cmd
-        $CORE_ROOT/libexec/dvtm/dvtm-opener
+        $CORE_ROOT/libexec/dvtm/dvtm-open
         $CORE_ROOT/libexec/emacs/ec
         $CORE_ROOT/libexec/emacs/etags
     )
