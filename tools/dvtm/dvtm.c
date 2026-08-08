69d9fd9b550082361d9db973e48d0be1cca4d9e2
Zachary Zhou <zchrzhou@gmail.com>
2026-08-08 23:41:19 +0800

    dvtm: update dvtm.c on draw_border

--- tools/dvtm/dvtm.c
+++ tools/dvtm/dvtm.c
@@ -461,13 +461,12 @@ draw_border(Client *c) {
 	}
 
 	bool is_mod_active = (c == sel && mod_key_pressed && bar.pos == BAR_OFF);
-	char *sep = "/";
 
 	mvwprintw(c->window, 0, 2, "[%s%s%s%d%s%d]",
-	          is_mod_active ? "» " : "",
+	          is_mod_active ? "> " : "",
 	          (*c->title && showtitle) ? c->title : "",
 	          (*c->title && showtitle) ? " | " : "",
-	          c->order, sep, n);
+	          c->order, "/", n);
 	if (t)
 		c->title[maxlen] = t;
 	wmove(c->window, y, x);
