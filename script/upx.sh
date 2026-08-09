eceaff114b13e190865171f015c469d9f2506da5
Zachary Zhou <zchrzhou@gmail.com>
2026-08-09 11:56:10 +0800

    Update script/upx.sh

--- script/upx.sh
+++ script/upx.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 
 ## Compress the binary
-## $ upx --ultra-brute nnn
+## $ upx --ultra-brute nnn || upx --best nnn
 ## Revert it back to the original 1.3MB binary at any time
 ## $ upx -d nnn
 
 # 1. Download the official static release
-curl -LO https://github.com/upx/upx/releases/download/v5.2.0/upx-5.2.0-amd64_linux.tar.xz
+VER=5.2.0
+curl -LO https://github.com/upx/upx/releases/download/v${VER}/upx-${VER}-amd64_linux.tar.xz
 
 # 2. Extract the archive
-tar -xf upx-4.2.2-amd64_linux.tar.xz
+tar -xf upx-${VER}-amd64_linux.tar.xz
 
 # 3. Move the binary into your PATH
-sudo mv upx-4.2.2-amd64_linux/upx /usr/local/bin/
+sudo mv upx-${VER}-amd64_linux/upx /usr/local/bin/
 
 # 4. Clean up downloaded files
-rm -rf upx-4.2.2-amd64_linux*
+rm -rf upx-${VER}-amd64_linux*
